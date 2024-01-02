e764a4dc127af53d6080da74699e0ebc9429f7f5
AndSim <siminiucandrew@gmail.com>
2024-01-02 19:40:54 +0200

    feat:(App.jsx) build (Accordion) component. contain 4 (AccordionItem) can be opened only one and the rest closed (handleToggle) (icon)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-	return <h1>React</h1>;
+const faqs = [
+	{
+		title: 'Where are these chairs assembled?',
+		text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+	},
+	{
+		title: 'How long do I have to return my chair?',
+		text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+	},
+	{
+		title: 'Do you ship to countries outside the EU?',
+		text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+	},
+];
+
+export default function App() {
+	return (
+		<div>
+			<Accordion data={faqs} />
+		</div>
+	);
 }
 
-export default App;
+function Accordion({ data }) {
+	const [curOpen, setCurOpen] = useState(null);
+
+	return (
+		<div className="accordion">
+			{data.map((el, i) => (
+				<AccordionItem
+					curOpen={curOpen}
+					onOpen={setCurOpen}
+					title={el.title}
+					num={i}
+					key={el.title}
+				>
+					{el.text}
+				</AccordionItem>
+			))}
+
+			<AccordionItem
+				curOpen={curOpen}
+				onOpen={setCurOpen}
+				title="Allows React developers to:"
+				num={5}
+				key="react dev"
+			>
+				<ul>
+					<li>Break up UI into components</li>
+					<li>Make components reusuable</li>
+					<li>Place state efficiently</li>
+				</ul>
+			</AccordionItem>
+		</div>
+	);
+}
+
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+	const isOpen = num === curOpen;
+
+	function handleToggle() {
+		onOpen(isOpen ? null : num);
+	}
+
+	return (
+		<div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+			<p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+			<p className="title">{title}</p>
+			<p className="icon">{isOpen ? '-' : '+'}</p>
+
+			{isOpen && <div className="content-box">{children}</div>}
+		</div>
+	);
+}
